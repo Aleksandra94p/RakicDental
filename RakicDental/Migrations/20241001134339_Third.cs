72ac042a78ef5fbd01e6e8dc851daac8774ffffa
Aleksandra94p <168584812+Aleksandra94p@users.noreply.github.com>
2024-11-28 16:19:19 +0100

    adding-table

---
+++ RakicDental/Migrations/20241001134339_Third.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RakicDental.Migrations
+{
+    public partial class Third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastCheckupDate",
+                table: "Patients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastCheckupDate",
+                table: "Patients");
+        }
+    }
+}
