ca12f69dfd29ddaec11ed5eedabdd6778e82d378
Aleksandra94p <168584812+Aleksandra94p@users.noreply.github.com>
2024-11-28 18:01:27 +0100

    adding header

---
+++ rakic-dental-client/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+    return(
+        <header>
+        <div className="header-content">
+        <h3>Ordinacija Rakic</h3>
+        </div>
+        </header>
+    );
+
+}
+
+export default Header;
